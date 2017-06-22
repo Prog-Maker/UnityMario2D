baf0e22957fa597667f24c0b6c10f3476e054c5a
Prog-Maker <salihovin@mail.ru>
2017-06-22 16:42:07 +0500

    Добавлен хелпп класс с методами расширения для ускорения написания скриптов.

---
+++ Scripts/Managers/HelpsClass.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+
+namespace MarioWorldForAll
+{
+
+    public static class HelpsClass
+    {
+        /// <summary>
+        /// Метод задает localScale по оси X.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="scale"></param>
+        public static void SetLocalScaleX (this Transform transform, float scale)
+        {
+            var vector = new Vector3 (scale, transform.localScale.y, transform.localScale.z);
+
+            transform.localScale = vector;
+        }
+
+
+        /// <summary>
+        /// Метод задает localScale по оси Y.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="scale"></param>
+        public static void SetLocalScaleY (this Transform transform, float scale)
+        {
+            var vector = new Vector3 (transform.localScale.x, scale, transform.localScale.z);
+
+            transform.localScale = vector;
+        }
+
+        /// <summary>
+        /// Метод задает localScale по оси Z.
+        /// </summary>
+        /// <param name="vector"></param>
+        /// <param name="scale"></param>
+        public static void SetLocalScaleZ (this Transform transform, float scale)
+        {
+            var vector = new Vector3 (transform.localScale.x, transform.localScale.y, scale);
+
+            transform.localScale = vector;
+        }
+
+
+        public static void deBug (this object obj)
+        {
+            Debug.Log (obj);
+        }
+    }
+}
